9e948117c3f8ce343be4ab8d01c95029219616f2
Eric Mooney <eric.c.mooney@gmail.com>
2013-07-20 20:48:46 -0700

    Remove to_i to stop infinite loop

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -18,8 +18,8 @@ class DestinationsController < ApplicationController
     @destination.get_random_coordinates
 
     # make sure the destination has a valid city returned, delete errors SEE IF I CAN do this before saving a record to DB
-    if [0,1,2].include?(@destination.address.to_i)
-      until ![0,1,2].include?(@destination.address.to_i)
+    if [0,1,2].include?(@destination.address)
+      until ![0,1,2].include?(@destination.address)
         @destination.destroy
         @destination = Destination.new
         @destination.get_random_coordinates
